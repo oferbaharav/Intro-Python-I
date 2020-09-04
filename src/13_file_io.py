365b4e0d07fa825d785af8440d67febd670f07af
Ofer Baharav <oferbaharav@gmail.com>
2020-09-03 21:13:19 -0700

    first assignnment

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,8 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+with open('foo.txt') as f:
+    print(f.read())
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +19,7 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open('bar.txt', "w") as f:
+    f.write("Once upon a time there was a little frog called John\n")
+    f.write("Then the frog one day kissed a prince\n")
+    f.write("And the prince became gay\n")
